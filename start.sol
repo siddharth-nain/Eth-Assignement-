dcd0b65593c4cde0fd666b88f2e2f0c98ca87d17
siddharth-nain <124575984+siddharth-nain@users.noreply.github.com>
2024-06-11 19:38:31 +0530

    Update start.sol

--- start.sol
+++ start.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    constructor(string memory name, string memory symbol) ERC20("Siddharth", "SID") Ownable(msg.sender) {}
+    constructor(string memory name, string memory symbol) ERC20("SIDDHARTH", "SID") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
@@ -13,5 +13,17 @@ contract MyToken is ERC20, Ownable {
 
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
+    }
+
+    
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        address sender = _msgSender();
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(balanceOf(sender) >= amount, "ERC20: transfer amount exceeds balance");
+
+        
+        _transfer(sender, recipient, amount);
+
+        return true;
     }
 }
